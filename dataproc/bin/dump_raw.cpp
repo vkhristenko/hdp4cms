9fc7e5d6629a8d24dee4596cb3e45a4023dc0419
vkhristenko <vdkhristenko1991@gmail.com>
2018-03-09 18:03:05 +0100

    cmake works

---
+++ dataproc/bin/dump_raw.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#define USEROOT
+#define THRESHOLD 10
+//##undef USEROOT
+
+#ifdef USEROOT
+#include "TFile.h"
+#include "TTree.h"
+#endif
+
+#define PRINT(x) \
+    std::cout << #x " = " << x  << std::endl
+
+#define PRINTRAW(x) \
+    printf(#x " = %02x\n", x);
+
+// type decls
+using TRawBuffer = std::vector<unsigned char>;
+using TRawDataCollection = std::vector<TRawBuffer>;
+
+namespace common {
+
+void dump_raw(TRawBuffer const& buffer) {
+    auto counter = 0;
+    for (auto it = buffer.begin(); it!=buffer.end(); counter++, ++it) {
+        if (counter % 25 == 0) 
+            printf("\n");
+        printf("%02x ", *it);
+    }
+    printf("\n\n");
+}
+
+}
+
+namespace hcal {
+
+class module_header {
+public:
+    inline uint16_t amcid() const { return uint16_t(header & 0xFFFF); }
+    inline int amcslot() const { return int((header >> 16) & 0xF); }
+    inline int amcblocknumber() const { return int((header >> 20) & 0xFF); }
+    inline int amcsize() const { return int((header >> 32) & 0xFFFFFF); }
+    inline bool amcmore() const { return ((header >> 61) & 0x1) != 0; }
+    inline bool amcsegmented() const { return ((header >> 60) & 0x1) != 0; }
+    inline bool amclengthok() const { return ((header >> 62) & 0x1) != 0; }
+    inline bool amccrok() const { return ((header  >> 56) & 0x1) != 0 ;}
+    inline bool amcdatapresent() const { return ((header >> 58) & 0x1)!=0; }
+    inline bool amcdatavalid() const { return ((header >> 57) & 0x1)!=0; }
+    inline bool amcenabled() const { return ((header >> 59) & 0x1)!=0; }
+
+private:
+    uint64_t header;
+};
+
+class amc13header {
+public:
+    inline int namc() const { return int((amc13_header >> 52) & 0x0F); }
+    inline int amc13formatversion() const { return int((amc13_header >> 60) & 0xF); }
+    inline int sourceid() const { return int((cdf_header >> 8) & 0xFFF); }
+    inline int bunchid() const { return int((cdf_header >> 20) & 0xFFF); }
+    inline int l1anumber() const { return int((cdf_header >> 32) & 0x00FFFFFF); }
+    inline int cdfeventtype() const { return int((cdf_header >> 56) & 0x0F); }
+    inline unsigned int orbitnumber() const { return (unsigned int)((amc13_header >> 4) & 0xFFFFFFFFu); }
+
+    inline module_header const& getm(int i) const { return modules_headers[i]; }
+
+private:
+    uint64_t cdf_header;
+    uint64_t amc13_header;
+    module_header modules_headers[12];
+};
+
+void unpack_utca(TRawBuffer const& buffer) {
+    // raw buffer 
+    unsigned char const *data = &buffer[0];
+
+    // emulate the cmssw process
+    amc13header const *header = (amc13header const*)data;
+
+    PRINT(header->namc());
+    PRINT(header->amc13formatversion());
+    PRINT(header->sourceid());
+    PRINT(header->bunchid());
+    PRINT(header->l1anumber());
+    PRINT(header->cdfeventtype());
+    PRINT(header->orbitnumber());
+
+    for (auto iamc=0; iamc<header->namc(); iamc++) {
+        PRINT(iamc);
+        PRINT(header->getm(iamc).amcid());
+        PRINT(header->getm(iamc).amcslot());
+        PRINT(header->getm(iamc).amcblocknumber());
+        PRINT(header->getm(iamc).amcsize());
+        PRINT(header->getm(iamc).amcmore());
+        PRINT(header->getm(iamc).amcsegmented());
+        PRINT(header->getm(iamc).amclengthok());
+        PRINT(header->getm(iamc).amccrok());
+        PRINT(header->getm(iamc).amcdatapresent());
+        PRINT(header->getm(iamc).amcenabled());
+    }
+
+    // num of uhtrs in this FED
+    /*
+    auto nuhtrs = header->namc();
+    for (auto iuhtr=0; i<nuhtrs; i++) {
+
+    }*/
+}
+
+void unpack(TRawBuffer const& buffer) {
+    PRINT(buffer.size());
+
+    // pointer to the first byte of the buffer
+    unsigned char const *data = &buffer[0];
+
+    // BOEshouldBeZeroAlways() const { return ( (commondataformat3>>28) & 0x0F); }
+    // /** Get the Beginning Of Event bits.  If it's not the first or last CDF Slink64 word, the high 4 bits must be zero.*/
+    short boe = (data[12] >> 4) & 0x0F;
+    PRINTRAW(data[12]);
+    PRINTRAW(boe);
+
+    if (boe == 0) {
+        printf("vme fed found\n");
+        return;
+    }
+    unpack_utca(buffer);
+
+    // dump the whole buffer
+    common::dump_raw(buffer);
+}
+
+}// end namespace hcal
+
+int main(int argc, char ** argv) {
+    std::cout << "hello world" << std::endl;
+
+    // parse the args
+    std::string pathToFile(argv[1]);
+
+    // validate args
+    PRINT(pathToFile);
+
+    TFile *f = new TFile(pathToFile.c_str());
+    TTree *tree = (TTree*)f->Get("getraw/Events");
+
+    // for debug
+    PRINT(tree->GetEntries());
+
+    TRawDataCollection *raw = nullptr;
+
+    tree->SetBranchAddress("RawData", &raw);
+
+    int nevents = tree->GetEntries();
+    for (auto i=0; i<nevents && i<THRESHOLD; i++) {
+        tree->GetEntry(i);
+        PRINT(raw->size());
+
+        for (auto it=raw->begin(); it!=raw->end(); ++it) {
+            if (it->size() == 0) {
+                printf("skipping fed\n");
+                continue;
+            }
+            hcal::unpack(*it);
+        }
+    }
+
+    f->Close();
+}
---
+++ dataproc/conf/LinkDef.h
@@ -0,0 +1,10 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class std::vector<unsigned char>+;
+#pragma link C++ class std::vector<std::vector<unsigned char> >+;
+
+#endif
