9015826b4b512e47e16c70a233abb59a98899c68
vkhristenko <vdkhristenko1991@gmail.com>
2018-03-13 23:00:00 +0100

    able to read cmssw raw files directly. dictionaries are working properly

--- dataproc/conf/LinkDef.h
+++ dataproc/conf/LinkDef.h
@@ -7,4 +7,11 @@
 #pragma link C++ class std::vector<unsigned char>+;
 #pragma link C++ class std::vector<std::vector<unsigned char> >+;
 
+#pragma link C++ class edm::DoNotRecordParents+;
+#pragma link C++ class FEDRawData+;
+#pragma link C++ class FEDRawDataCollection+;
+#pragma link C++ class edm::ViewTypeChecker+;
+#pragma link C++ class edm::WrapperBase+;
+#pragma link C++ class edm::Wrapper<FEDRawDataCollection>+;
+
 #endif
