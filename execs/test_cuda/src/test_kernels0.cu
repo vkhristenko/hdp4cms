61c48ce19a2d3be65a94acab84ba130539f73ff7
Viktor Khristenko <vdkhristenko1991@gmail.com>
2018-04-25 17:13:17 +0200

    further work on vinavx2

--- execs/test_cuda/src/test_kernels0.cu
+++ execs/test_cuda/src/test_kernels0.cu
@@ -1,7 +1,13 @@
+#include "execs/test_cuda/interface/test_kernels0.h"
+
 //
 // simple vector addition example
 //
-__global__ void vector_add(int* a, int* b, int* c) {
+__global__ void cu_vector_add(int* a, int* b, int* c) {
     int id = blockIdx.x;
     c[id] = a[id] + b[id];
+}
+
+void vector_add(int *a, int* b, int* c, int const n) {
+    cu_vector_add<<<n, 1>>>(a, b, c);
 }
